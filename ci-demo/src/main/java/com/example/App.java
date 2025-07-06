da86c3dbdafc749163532257e3342a820432a2b1
ShikhaCity123 <2021wa86042@wilp.bits-pilani.ac.in>
2025-07-07 02:29:05 +0530

    Update App.java
    Updated App.java for CI testing

--- ci-demo/src/main/java/com/example/App.java
+++ ci-demo/src/main/java/com/example/App.java
@@ -1,13 +1,5 @@
-package com.example;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI! -testing performed by Shikha Yadav (2021WA86042)");
     }
 }
